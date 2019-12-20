361ab80e0a505b6e1ad79bda0915d40b09f60f2a
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2019-12-20 18:45:24 +0100

    Horizontale Linien werden gezeichnet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,6 +21,13 @@ public class Main extends PApplet {
     }
 
     private void drawSudoku() {
-        rect((int)(windowSize/6), (int)(windowSize/40), (int)(windowSize*2/3), (int)(windowSize*2/3));
+
+        int sudokuSize = (int)(windowSize*2/3);
+
+        rect((int)(windowSize/6), (int)(windowSize/40), sudokuSize, sudokuSize);
+
+        for (int i = 1; i < 9; i++) {
+            line((int) ((sudokuSize/9)*i + windowSize/6), (int)(windowSize/40), (int) ((sudokuSize/9)*i + windowSize/6), (int)(windowSize/40)+sudokuSize);
+        }
     }
 }
