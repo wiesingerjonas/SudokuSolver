08076e257bbdfc23438518110873b3a00c864051
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2019-12-20 18:39:38 +0100

    Grundform wird gezeichnet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,21 +2,25 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
 
+    private int windowSize = 800;
+
     public static void main(String[] args) {
-        PApplet.main("MainClass", args);
+        PApplet.main("Main", args);
     }
 
     public void settings() {
-        size(800, 800);
-
+        size(windowSize, windowSize);
     }
 
     public void setup() {
         frameRate(60);
-
     }
 
     public void draw() {
+        drawSudoku();
+    }
 
+    private void drawSudoku() {
+        rect((int)(windowSize/6), (int)(windowSize/40), (int)(windowSize*2/3), (int)(windowSize*2/3));
     }
 }
