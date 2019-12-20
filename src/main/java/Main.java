55c16405c9845614c0362c1649610396a1d2a6ea
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2019-12-20 18:24:36 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("MainClass", args);
+    }
+
+    public void settings() {
+        size(800, 800);
+
+    }
+
+    public void setup() {
+        frameRate(60);
+
+    }
+
+    public void draw() {
+
+    }
+}
