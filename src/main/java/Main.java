4119fe2f63008bb46a5966b54d7a0ab193d02362
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2019-12-20 18:56:48 +0100

    drawSudoku() ist fertig

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -24,20 +24,42 @@ public class Main extends PApplet {
 
         int sudokuSize = (windowSize*2/3);
 
+        strokeWeight(3);
         rect((int)(windowSize/6), (int)(windowSize/40), sudokuSize, sudokuSize);
 
         for (int i = 1; i < 9; i++) {
 
+            strokeWeight(1);
+
+            //horizontal
             line(   (int) ((sudokuSize/9)*i + windowSize/6),
                     (int)(windowSize/40),
                     (int) ((sudokuSize/9)*i + windowSize/6),
                     (int)(windowSize/40)+sudokuSize);
 
+            //vertikal
             line(   (int)(windowSize/6),
                     (int) ((sudokuSize/9)*i + windowSize/40),
                     (int)(windowSize/6 + sudokuSize),
                     (int) ((sudokuSize/9)*i + windowSize/40));
 
+        }
+
+        for (int i = 1; i < 3; i++) {
+
+            strokeWeight(3);
+
+            //horizontal
+            line(   (int) ((sudokuSize/3)*i + windowSize/6),
+                    (int)(windowSize/40),
+                    (int) ((sudokuSize/3)*i + windowSize/6),
+                    (int)(windowSize/40)+sudokuSize);
+
+            //vertikal
+            line(   (int)(windowSize/6),
+                    (int) ((sudokuSize/3)*i + windowSize/40),
+                    (int)(windowSize/6 + sudokuSize),
+                    (int) ((sudokuSize/3)*i + windowSize/40));
         }
     }
 }
