213b0c5a3165effe1525666a91d590cff94a454d
wiesingerjonas <j.wiesinger@students.htl-leonding.ac.at>
2020-01-06 15:55:58 +0100

    neue Klasse Field implementiert

---
+++ src/main/java/Field.java
@@ -0,0 +1,12 @@
+public class Field {
+
+    private int value = -1;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
